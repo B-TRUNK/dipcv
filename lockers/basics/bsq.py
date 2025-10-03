c3ca23c99c1cbc6b93ebe18fd7381a28ce443ce5
abanoyb_boshra <abanoub.boshra.anis.com>
2025-10-03 21:04:03 +0300

    dep_inst

--- lockers/basics/bsq.py
+++ lockers/basics/bsq.py
@@ -1,11 +1,12 @@
 import cv2
 from skimage.filters import sobel
 
-img = cv2.imread("raw_imgs/hq.jpg" ,1)
+img = cv2.imread("../../raw_imgs/hq.jpg" ,1)
 img2 = sobel(img) 
 
 
 cv2.imshow("pic" ,img)
+cv2.imshow("edge" ,img2)
 print(img.shape)
 
 cv2.waitKey(0)
