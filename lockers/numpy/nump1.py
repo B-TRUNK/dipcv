056d33da396e5b251a26d21f03508334cb3d3f77
abanoyb_boshra <abanoub.boshra.anis.com>
2025-10-04 20:39:27 +0300

    numpy

---
+++ lockers/numpy/nump1.py
@@ -0,0 +1,65 @@
+import numpy as np
+import cv2
+
+
+a = [1 ,2 ,3 ,4 ,5]
+print(a*2)
+print("Before Using Numpy")
+
+
+
+b = np.array(a)
+print(b*2)
+print("After Using Numpy")
+
+#use numpy to generate an array
+zero_matrix = np.zeros((2 ,2))
+print("======== \n zero matrix : \n ======== \n" ,zero_matrix)
+
+custom_matrix = np.full((3 ,3) ,4)
+print("======== \n custom matrix : \n ======== \n" ,custom_matrix)
+
+identity_matrix = np.eye(3)
+print("======== \n identity matrix : \n ======== \n" ,identity_matrix)
+
+random_matrix = np.random.random((2 ,2))
+print("======== \n random matrix : \n ======== \n" ,random_matrix)
+
+#=======================================================================
+#slicing a matrix
+a = np.array( [ [1 ,2 ,3 ,4] ,[5 ,6 ,7 ,8] ,[9 ,10 ,11 ,12] ] )
+print(a)
+
+b = a[:2]
+print(b)
+
+#========================================================================
+#array indexing
+print(a[0 ,0] ,a[1,1] ,a [2, 0])
+
+#========================================================================
+#MATRIX OPERATIONS
+
+x = np.array( [[1 ,2] ,[3 ,4]] ,dtype = np.float64 )
+y = np.array( [[5 ,6] ,[7 ,8]] ,dtype = np.float64 )
+
+print("======== \n Matrices_MUL : \n ======== \n" ,np.multiply(x ,y))
+print("======== \n Matrices_SUM : \n ======== \n" ,np.add(x ,y))
+print("======== \n Matrices_SUM-at a spicific axis : \n ======== \n" ,np.sum(x ,axis=0))
+print("======== \n Matrix-Transpose : \n ======== \n" ,x.T)
+
+#=========================================================================
+#Real Operations on an Image
+img = cv2.imread("../../raw_imgs/hq.jpg" ,1)
+
+cv2.imshow("pic" ,img)
+print(img)
+
+tinted_img = (img * [1. ,0. ,0.])
+cv2.imshow("tinted" ,tinted_img)
+print(tinted_img)
+#cv2.imwrite("../../raw_imgs/hq-tinted-b.jpg" ,tinted_img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
